d59640da918033a12f39509e805e75b86482a970
KavingaW <kavinga@hsenidlanka.com>
2023-05-25 10:31:26 +0530

    update_git_workflows dev3 --

--- src/main/java/com/hsenid/surveyapp/controller/SurveyController.java
+++ src/main/java/com/hsenid/surveyapp/controller/SurveyController.java
@@ -9,7 +9,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//test oncurrency223899 w3dw
+//test oncurrency223899 w3dwss
 // 0
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
